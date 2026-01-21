afdad5d2479413754b64301041ddfa667b5fcd9f
Rushik005 <90761109+Rushik005@users.noreply.github.com>
2026-01-21 12:12:39 +0530

    Create main.tf

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,18 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = var.subnet_ids
+  vpc_id          = null
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.node_instance_type]
+      desired_size   = 2
+      min_size       = 2
+      max_size       = 4
+    }
+  }
+}
